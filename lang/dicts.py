52f767689eb8c28dc02dfd8e75ae6a8e3cfdb77b
ming <xiemingzhi@gmail.com>
2017-12-11 22:43:12 +0800

    Add to dict with set
    (cherry picked from commit f6024e7bcea7eee472f24d8d40fea8d7a3d90b65)

--- lang/dicts.py
+++ lang/dicts.py
@@ -1,8 +1,20 @@
 # read from file put into dictionary
 f = open('data.txt', 'r');
+userProductDict = {};
 
 for line in f:
-  print line;
-  
+  linearr = line.split("\t");
+  if linearr[0] != "User ID":
+    if linearr[0] in userProductDict:
+      #print(linearr[0] + " " + linearr[1]);
+      #print(userProductDict[linearr[0]]);
+      # type(userProductDict[linearr[0]]);
+      userProductDict[linearr[0]].add(linearr[1]);
+    else:
+      userProductDict[linearr[0]] = set([linearr[1]]);
+
+for user, products in userProductDict.items():
+  print (user, products);
+
 # close the file after reading
 f.close();
