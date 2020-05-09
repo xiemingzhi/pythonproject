48ba9ea35970c2eadc982d6ddf814b84ba064946
ming <xiemingzhi@gmail.com>
2020-05-09 22:33:53 +0800

    Update http-request example

--- network/http-request.py
+++ network/http-request.py
@@ -5,7 +5,8 @@
 import json
 import requests
 
-url = 'http://192.168.11.17:8080/springwebapp/rest/contact'
+#requests.exceptions.SSLError: HTTPSConnectionPool(host='jsonplaceholder.typicode.com', port=443): Max retries exceeded with url: /todos?_limit=1 (Caused by SSLError("Can't connect to HTTPS URL because the SSL module is not available."))
+url = 'http://jsonplaceholder.typicode.com/todos?_limit=1'
 payload = {'firstName': 'myname', 'lastName': 'mylast', 'email':'myname@mylast.com'}
 
 r = requests.post(url, json=payload)
