3b6fbb72ab126761869fb8e7c97db3ffae3d492e
ming <xiemingzhi@gmail.com>
2020-05-16 10:22:57 +0800

    Create linkedlist example
    How to create an iterator in python
    How to self increment

---
+++ lang/linkedlist-manual.py
@@ -0,0 +1,56 @@
+class LinkedList:
+    def __init__(self):
+        self.node = None
+        self.count = 0
+
+    def add(self, data):
+        if self.node is None:
+            self.node = Node(data)
+            self.node.setNext(None)
+            self.count += 1
+        else:
+            oldnode = self.node 
+            self.node = Node(data)
+            self.node.setNext(oldnode)
+            self.count += 1
+
+    # creating a iterator in python
+    def __iter__(self):
+        self.nextIndex = self.node
+        self.totalCount = self.count
+        self.iterationCount = 0
+        return self
+
+    def __next__(self):
+        if (self.iterationCount < self.totalCount):
+            result = self.nextIndex
+            self.nextIndex = self.nextIndex.next
+            self.iterationCount += 1
+            return result
+        else: 
+            raise StopIteration
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+
+    def getData(self):
+        return self.data
+
+    def next(self):
+        return self.next
+
+    def setNext(self, next):
+        self.next = next
+
+linkedList = LinkedList() 
+linkedList.add('1')
+linkedList.add('2')
+linkedList.add('3')
+linkedList.add('4')
+
+llIter = iter(linkedList)
+for i in llIter:
+    print(i.getData())
+
