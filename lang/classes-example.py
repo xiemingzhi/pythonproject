dd04a0fa6a3827edb917a39877a9e48f1f572d27
ming <xiemingzhi@gmail.com>
2020-07-25 18:38:13 +0800

    Update class example
    __str__ print object
    __eq__ compare object

--- lang/classes-example.py
+++ lang/classes-example.py
@@ -2,7 +2,19 @@ class Person:
   def __init__(self, name, age):
     self.name = name
     self.age = age
-
+  def get_age(self):
+    return self.age
+  def get_name(self):
+    return self.name
+  def set_age(self, newage):
+    self.age = newage
+  def set_name(self, newname=""):
+    self.name = newname
+  def __str__(self):
+    return "person:" + str(self.name) + ":"+str(self.age)
+  def __eq__(self, other):
+    return self.name == other.name and self.age == other.age
+      
 p1 = Person("John", 36)
 
 print(p1.name)
